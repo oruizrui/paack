f0cf1746856f5bf5e885403cf03ab080e21abd94
oruizrui <oruizrui@gmail.com>
2020-03-22 15:51:59 +0100

    add data science -> average -> mean

---
+++ spec/mutations/data_science/average/mean_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe DataScience::Average::Mean do
+  subject { described_class.run!(params) }
+
+  let(:params) do
+    {
+        numbers: [3, 4, 5]
+    }
+  end
+
+  it 'expected results' do
+    expect(subject).to be_truthy
+    expect(subject).to eq(4.0)
+  end
+end
