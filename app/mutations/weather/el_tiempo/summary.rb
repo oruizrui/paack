4aacfcf4f1ded4889c4c0c1bbe13a1df56c37e6b
oruizrui <oruizrui@gmail.com>
2020-03-22 16:49:59 +0100

    add specs and command

--- app/mutations/weather/el_tiempo/summary.rb
+++ app/mutations/weather/el_tiempo/summary.rb
@@ -1,13 +1,39 @@
 class Weather::ElTiempo::Summary < Mutations::Command
 
   required do
-    array :tempmaxs
-    array :tempmins
+    hash :week do
+      array :tempmaxs, class: Numeric, min_length: 1
+      array :tempmins, class: Numeric, min_length: 1
+    end
     hash :today do
-      array :tempmaxs
-      array :tempmins
+      array :tempmaxs, class: Numeric, min_length: 1
+      array :tempmins, class: Numeric, min_length: 1
     end
   end
 
+  def execute
+    {
+        week: {
+            temp: {
+                av_max: av_temp(week[:tempmaxs]),
+                av_min: av_temp(week[:tempmins])
+            }
+        },
+        today: {
+            max: today[:tempmaxs].first,
+            min: today[:tempmins].first,
+            av: av_temp(today_temps)
+        }
+    }
+  end
 
+  private
+
+  def av_temp(numbers)
+    DataScience::Average::Mean.run!(numbers: numbers)
+  end
+
+  def today_temps
+    today.flat_map { |key, value| value }
+  end
 end
