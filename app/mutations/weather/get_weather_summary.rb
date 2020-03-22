bcf06949d5b34ada9799f5d25e03ff4fd283086e
oruizrui <oruizrui@gmail.com>
2020-03-22 16:55:59 +0100

    get weather summary

---
+++ app/mutations/weather/get_weather_summary.rb
@@ -0,0 +1,13 @@
+class GetWeatherSummary < Mutations::Command
+
+  required do
+    integer :city_id
+    string :provider, in: ['el-tiempo']
+  end
+
+  def execute
+    case provider
+    when 'el-tiempo' then GetSummary.run!(localidad: city_id)
+    end
+  end
+end
